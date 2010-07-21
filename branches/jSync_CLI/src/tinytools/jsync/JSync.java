32f1c14669e56d31d518bc93616226ccbc400769
eleph@users.sourceforge.net <eleph@users.sourceforge.net@d913dde4-e0cc-11dd-9ba5-89a75009fd5d>
2010-07-21 23:22:08 +0000

    implemented JavaSynchronizer

--- branches/jSync_CLI/src/tinytools/jsync/JSync.java
+++ branches/jSync_CLI/src/tinytools/jsync/JSync.java
@@ -1,8 +1,6 @@
 package tinytools.jsync;
 
-import java.io.File;
 import java.io.IOException;
-import java.util.List;
 
 import uk.co.flamingpenguin.jewel.cli.ArgumentValidationException;
 import uk.co.flamingpenguin.jewel.cli.CliFactory;
@@ -33,27 +31,27 @@ public class JSync
 		slave.indexFiles();
 
 		master.compareToSlave(slave);
-
-		if(!options.isDeleteOnSlave())
-		{
-			if(master.getChanges().deletedFiles != null)
-			{
-				List<File> deletedFiles = master.getChanges().deletedFiles;
-				System.out.println(deletedFiles.size() + " files have been deleted on master. This changes will be ignored. Use --deleteOnSlave to do a full sync.");
-				master.getChanges().deletedFiles.clear();
-			}
-		}
+		
+		ISynchronizer synchronizer;
 		
 		/* should we only generate a shell script? */
 		if (options.isShellScriptFile())
 		{
-			generateShellScript(options, master, slave);
-			System.exit(0);
+			synchronizer = new ShellScriptGenerator();
+		}
+		else
+		{
+			synchronizer = new JavaSynchronizer();
 		}
 		
-
+		synchronizer.setOptions(options);
+		synchronizer.setChanges(master.getChanges());
+		synchronizer.setMasterDir(master.getPath());
+		synchronizer.setSlaveDir(slave.getPath());
+		
+		synchronizer.synchronize();
 	}
-
+	
 	private static void printHelp(ArgumentValidationException ex)
 	{
 		System.err.println(ex.getMessage());
@@ -62,21 +60,5 @@ public class JSync
 		System.out.println("Use --help for help");
 		System.out.flush();
 		System.exit(1);
-	}
-
-	private static void generateShellScript(Options options, FileFinder master,
-			FileFinder slave) throws Exception
-	{
-		File shellScriptFile = new File(options.getShellScriptFile());
-		System.out
-				.println("Shell script with sync commands will be written to: "
-						+ shellScriptFile.getAbsolutePath());
-
-		ShellScriptGenerator generator = new ShellScriptGenerator();
-		generator.setOptions(options);
-		generator.setChanges(master.getChanges());
-		generator.setMasterDir(master.getPath());
-		generator.setSlaveDir(slave.getPath());
-		generator.synchronize();
 	}
 }
