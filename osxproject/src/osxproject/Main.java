bc541b67e16557451a773b661ffdafb1014b60b2
eleph <eleph@users.sourceforge.net>
2011-01-02 23:42:53 +0000

    made icon better visible through outline

--- osxproject/src/osxproject/Main.java
+++ osxproject/src/osxproject/Main.java
@@ -1,10 +1,15 @@
 package osxproject;
 
+import java.awt.Frame;
+
 public class Main
 {
 	public static void main(String[] args) throws Exception
 	{
 		System.out.println("running...");
-		System.in.read();
+		
+		Frame f = new Frame("TestFrame");
+		f.setSize(500, 200);
+		f.show();
 	}
 }
