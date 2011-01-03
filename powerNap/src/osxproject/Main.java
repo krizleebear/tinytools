8de94f397252d4eccf13ab56fb00e6a730a1a6e5
eleph <eleph@users.sourceforge.net>
2011-01-03 00:00:49 +0000

    copied common osx project to new project 'power nap'

---
+++ powerNap/src/osxproject/Main.java
@@ -0,0 +1,15 @@
+package osxproject;
+
+import java.awt.Frame;
+
+public class Main
+{
+	public static void main(String[] args) throws Exception
+	{
+		System.out.println("running...");
+		
+		Frame f = new Frame("TestFrame");
+		f.setSize(500, 200);
+		f.show();
+	}
+}
