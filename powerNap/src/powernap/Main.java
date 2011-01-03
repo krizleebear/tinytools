beebe795fa1a4f9bff1fc8dbe8cf81aba4c39827
eleph <eleph@users.sourceforge.net>
2011-01-03 01:27:40 +0000

    added new icon set

---
+++ powerNap/src/powernap/Main.java
@@ -0,0 +1,30 @@
+package powernap;
+
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import javax.swing.JFrame;
+import javax.swing.UIManager;
+
+public class Main
+{
+	public static void main(String[] args) throws Exception
+	{
+		System.out.println("running...");
+
+		UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+
+		final JFrame f = new JFrame("PowerNap");
+		f.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);		
+		f.setSize(256, 256);
+		f.setResizable(false);
+		f.setVisible(true);
+		f.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent arg0)
+			{
+				System.out.println("closing");
+				//f.setVisible(false);
+			}
+		});
+	}
+}
